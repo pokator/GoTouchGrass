8ce21e5bd197c359ff84b0314579ab82eed271f5
Kayla Han <kaylahan@Elinores-MacBook-Air.local>
2024-03-16 21:02:53 -0500

    added username setting

--- GoTouchGrass/RegisterViewController.swift
+++ GoTouchGrass/RegisterViewController.swift
@@ -14,8 +14,6 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     
-    @IBOutlet weak var usernameField: UITextField!
-    
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet weak var confirmPassField: UITextField!
@@ -32,14 +30,14 @@ class RegisterViewController: UIViewController {
     }
 
     @IBAction func onRegisterPressed(_ sender: Any) {
-        if (((emailField.text?.isEmpty) == false) && ((usernameField.text?.isEmpty) == false) && ((passwordField.text?.isEmpty) == false)) {
+        if (((emailField.text?.isEmpty) == false) && ((passwordField.text?.isEmpty) == false)) {
             let name = emailField.text!
             let pass = passwordField.text!
             if (!name.isEmpty && !pass.isEmpty && (pass == confirmPassField.text)) {
                 //can register
                 validRegister = true
                 Auth.auth().createUser(withEmail: emailField.text!,
-                                       password: passwordField.text!) {
+                                   password: passwordField.text!) {
                     (authResult, error) in
                     if let error = error as NSError? {
                         self.statusLabel.text = "\(error.localizedDescription)"
@@ -47,13 +45,6 @@ class RegisterViewController: UIViewController {
                         self.statusLabel.text = ""
                     }
                 }
-                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-                print("Changing username", usernameField.text!)
-                changeRequest?.displayName = usernameField.text!
-                changeRequest?.commitChanges { error in
-                    // ...
-                }
-                Auth.auth().currentUser?.reload()
             }
         }
     }
