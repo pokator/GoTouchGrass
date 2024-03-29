050ec08bbb45329b56de1407c3ffa62ea8df24ff
Kayla <kaylarhan@gmail.com>
2024-03-29 17:41:20 -0500

    added data test view

---
+++ DatabaseViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DatabaseViewController.swift
+//  GoTouchGrass
+//
+//  Created by Kayla Han on 3/29/24.
+//
+
+import UIKit
+
+class DatabaseViewController: UIViewController {
+
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    @IBOutlet weak var dataField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onWritePressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func onReadPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
