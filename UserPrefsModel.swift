c5f9fa2533cac9bcea8f4cea09ccbaefbb10aa8f
Kayla <kaylarhan@gmail.com>
2024-03-29 18:32:23 -0500

    basic read and write

---
+++ UserPrefsModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserPrefsModel.swift
+//  GoTouchGrass
+//
+//  Created by Kayla Han on 3/29/24.
+//
+
+import Foundation
+
+struct UserPrefsModel: Identifiable, Codable {
+  var id: String?
+  var text: String
+}
